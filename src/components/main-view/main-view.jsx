e70ddf490d60dea7fa289daff48c5f7cccef8554
I-Sammy <samiaislam@outlook.com>
2022-02-27 13:58:53 +1100

    updated img links

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -11,9 +11,9 @@ export class MainView extends React.Component {
     super();
     this.state = {
       movies: [
-        { _id: 1, Title: 'Inception', Description: 'Inception is a 2010 science fiction action film written and directed by Christopher Nolan, who also produced the film with Emma Thomas, his wife. The film stars Leonardo DiCaprio as a professional thief who steals information by infiltrating the subconscious of his targets.', ImagePath: 'https://en.wikipedia.org/wiki/Inception#/media/File:Inception_(2010)_theatrical_poster.jpg', Genre: 'Action, Sci-fi', Director: 'Christopher Nolan' },
-        { _id: 2, Title: 'Iron Man', Description: 'Iron Man is a superhero appearing in American comic books published by Marvel Comics.', ImagePath: 'https://en.wikipedia.org/wiki/Iron_Man_(2008_film)#/media/File:Iron_Man_(2008_film)_poster.jpg', Genre: 'Action, Sci-fi', Director: 'Jon Favreau' },
-        { _id: 3, Title: 'Maleficent', Description: 'Maleficent (Angelina Jolie) rises to protect her peaceful forest kingdom from invaders, but a terrible betrayal turns her pure heart to stone and twists her into a creature bent on revenge.', ImagePath: 'https://en.wikipedia.org/wiki/Maleficent_(film)#/media/File:Maleficent_poster.jpg', Genre: 'Action, Fantasy', Director: 'Joachim Rønning' }
+        { _id: 1, Title: 'Inception', Description: 'Inception is a 2010 science fiction action film written and directed by Christopher Nolan, who also produced the film with Emma Thomas, his wife. The film stars Leonardo DiCaprio as a professional thief who steals information by infiltrating the subconscious of his targets.', ImagePath: 'https://upload.wikimedia.org/wikipedia/en/2/2e/Inception_%282010%29_theatrical_poster.jpg', Genre: 'Action, Sci-fi', Director: 'Christopher Nolan' },
+        { _id: 2, Title: 'Iron Man', Description: 'Iron Man is a superhero appearing in American comic books published by Marvel Comics.', ImagePath: 'https://upload.wikimedia.org/wikipedia/en/0/02/Iron_Man_%282008_film%29_poster.jpg', Genre: 'Action, Sci-fi', Director: 'Jon Favreau' },
+        { _id: 3, Title: 'Maleficent', Description: 'Maleficent (Angelina Jolie) rises to protect her peaceful forest kingdom from invaders, but a terrible betrayal turns her pure heart to stone and twists her into a creature bent on revenge.', ImagePath: 'https://upload.wikimedia.org/wikipedia/en/5/55/Maleficent_poster.jpg', Genre: 'Action, Fantasy', Director: 'Joachim Rønning' }
       ],
       selectedMovie: null
     };
